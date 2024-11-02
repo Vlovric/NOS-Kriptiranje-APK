4ff41cbbbbbffb0157717f9c896a0917fdaf2e97
Viktor Lovrić <viktorlovric21@gmail.com>
2024-11-03 00:59:02 +0100

    Napravljeni ekrani za biranje metode kriptiranja/dekriptiranja i postavljen css file sa bojama

--- Generiranje/generiranje.js
+++ Generiranje/generiranje.js
@@ -2,14 +2,14 @@ const { ipcRenderer } = require('electron');
 const crypto = require('crypto');
 
 document.addEventListener("DOMContentLoaded", function() {
-    // generirat tajni kljuc, prikazat u textarea i zapisat u file
+    // Napravit sa AES 256 ALGO
     var secretKey = generateSecretKey();
     var secretKeyTextArea = document.getElementById('tajniKljuc');
     secretKeyTextArea.value = secretKey;
     ipcRenderer.send('write-to-file', { fileType: 'tajni_kljuc', data: secretKey });
 
     //generirat javni i privatni kljuc, prikazat u textarea i zapisat u file
-    
+
 });
 
 function generateSecretKey(){
