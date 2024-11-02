36f103b29988a0cfca8add466936875860cab921
Viktor Lovrić <viktorlovric21@gmail.com>
2024-11-02 18:23:17 +0100

    Napravljene funkcije za komunikaciju sa fileovima

--- main.js
+++ main.js
@@ -2,49 +2,23 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const fs = require('fs-extra')
 
-app.whenReady().then(() => {
-  createWindow()
+const files = [
+  'kriptirani_tekst.txt',
+  'potpis.txt',
+  'sazetak.txt',
+  'javni_kljuc.txt',
+  'privatni_kljuc.txt',
+  'tajni_kljuc.txt',
+];
 
-  const files = [
-    'kriptirani_tekst.txt',
-    'potpis.txt',
-    'sazetak.txt',
-    'javni_kljuc.txt',
-    'privatni_kljuc.txt',
-    'tajni_kljuc.txt',
-  ];
-  const dir = path.join(app.getPath('userData'), 'files');
-  fs.ensureDirSync(dir);
-
-  files.forEach((file) => {
-    const filePath = path.join(dir, file);
-    fs.writeFileSync(filePath, '');
-  });
-
-  ipcMain.on('write-to-file', (event, { fileType, data }) => {
-    const fileMap = {
-      kriptirani_tekst: 'kriptirani_tekst.txt',
-      potpis: 'potpis.txt',
-      sazetak: 'sazetak.txt',
-      javni_kljuc: 'javni_kljuc.txt',
-      privatni_kljuc: 'privatni_kljuc.txt',
-      tajni_kljuc: 'tajni_kljuc.txt',
-    };
-
-    const fileName = fileMap[fileType];
-    if(!fileName){
-      event.reply('write-to-file-reply', 'ne postoji takav file');
-      return;
-    }
-    const filePath = path.join(dir, fileName);
-    fs.writeFileSync(filePath, data);
-    event.reply('write-to-file-reply', 'uspjesno');
-  });
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+const fileMap = {
+  kriptirani_tekst: 'kriptirani_tekst.txt',
+  potpis: 'potpis.txt',
+  sazetak: 'sazetak.txt',
+  javni_kljuc: 'javni_kljuc.txt',
+  privatni_kljuc: 'privatni_kljuc.txt',
+  tajni_kljuc: 'tajni_kljuc.txt',
+};
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -58,6 +32,56 @@ const createWindow = () => {
 
   win.loadFile('Pocetni/index.html')
 }
+
+function initializeFiles(){
+  const dir = path.join(app.getPath('userData'), 'files');
+  fs.ensureDirSync(dir);
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    fs.writeFileSync(filePath, '');
+  });
+}
+
+function writeToFile(){
+  ipcMain.on('write-to-file', (event, { fileType, data }) => {
+
+    const fileName = fileMap[fileType];
+    if(!fileName){
+      event.reply('write-to-file-reply', 'ne postoji takav file');
+      return;
+    }
+    const filePath = path.join(dir, fileName);
+    fs.writeFileSync(filePath, data);
+    event.reply('write-to-file-reply', 'uspjesno');
+  });
+}
+
+function readFromFile(){
+  ipcMain.on('read-from-file', (event, { fileType }) => {
+    const fileName = fileMap[fileType];
+    if(!fileName){
+      event.reply('read-from-file-reply', 'ne postoji takav file');
+      return;
+    }
+    const filePath = path.join(dir, fileName);
+    const data = fs.readFileSync(filePath);
+    event.reply('read-from-file-reply', data);
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  initializeFiles();
+  writeToFile();
+  readFromFile();
+
+  
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
