25d6dad7d724ffb79594620855e361ab5046f9ea
Viktor Lovrić <viktorlovric21@gmail.com>
2024-11-03 18:11:50 +0100

    Napravljeno kriptiranje

--- Kriptiranje/kriptiranje_simetricno.js
+++ Kriptiranje/kriptiranje_simetricno.js
@@ -20,25 +20,18 @@ document.addEventListener("DOMContentLoaded", function() {
         return new Promise((resolve, reject) => {
             ipcRenderer.send('read-from-file', { fileType: 'tajni_kljuc' });
             ipcRenderer.once('read-from-file-reply', (event, data) => {
-                if (data === 'ne postoji takav file') {
-                    reject('File does not exist');
-                } else {
-                    try {
-                        const key = Buffer.from(data.trim(), 'hex'); // Ensure the key is a 32-byte buffer
-                        if (key.length !== 32) {
-                            throw new Error('Invalid key length');
-                        }
-                        const iv = crypto.randomBytes(16); // Correct IV length
-                        const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-                        let encrypted = cipher.update(text, 'utf8', 'hex');
-                        encrypted += cipher.final('hex');
-                        const encryptedText = iv.toString('hex') + ':' + encrypted;
-                        console.log("Kriptirani tekst je: " + encryptedText);
-                        ipcRenderer.send('write-to-file', { fileType: 'kriptirani_tekst', data: encryptedText });
-                        resolve(encryptedText);
-                    } catch (error) {
-                        reject(error);
-                    }
+                try {
+                    const key = Buffer.from(data.trim(), 'hex'); // Ensure the key is a 32-byte buffer
+                    const iv = crypto.randomBytes(16); // Correct IV length
+                    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+                    let encrypted = cipher.update(text, 'utf8', 'hex');
+                    encrypted += cipher.final('hex');
+                    const encryptedText = iv.toString('hex') + ':' + encrypted;
+                    console.log("Kriptirani tekst je: " + encryptedText);
+                    ipcRenderer.send('write-to-file', { fileType: 'kriptirani_tekst', data: encryptedText });
+                    resolve(encryptedText);
+                } catch (error) {
+                    reject(error);
                 }
             });
         });
